8d862f508f8df2b194e268da78ce611d188ecd30
Kiril Peyanski <peyanski@progress.com>
2018-10-12 12:58:06 +0300

    feat: update panelbar to support selection of every item

--- src/components/PanelBarContainer.jsx
+++ src/components/PanelBarContainer.jsx
@@ -1,81 +1,112 @@
 import React from 'react';
 import { PanelBar, PanelBarItem } from '@progress/kendo-react-layout';
-export default class PanelBarContainer extends React.Component {
-    imageUrl(imageName) {
+import { classNames } from '@progress/kendo-react-common';
+class CustomPanelBarItem extends React.Component {
+    imageUrl = (imageName) => {
         return window.baseUrl + "/images/k3-sample-app/" + imageName + '.png';
     }
 
-    handleItemClick = (element) => {
-        document.getElementById(this.props.activeItem).classList.remove("k-state-selected")
-        this.props.changeData(element)
+    handleItemSelect = () => {
+        this.props.onClick.call(undefined, { target: this });
+    }
+    render() {
+        const className = classNames('teamMate', {
+            'k-state-selected': this.props.selected === this.props.uniquePrivateKey
+        });
+
+        return (
+            <div className={className} id={this.props.id} onClick={this.handleItemSelect}>
+                <img src={this.imageUrl(this.props.smallName)} alt={this.props.title} />
+                <span className="mate-info">
+                    <h2>{this.props.title}</h2>
+                    <p>{this.props.subTitle}</p>
+                </span>
+            </div>
+        )
+    }
+}
+export default class PanelBarContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: '.0.0'
+        }
+    }
+    handleItemSelect = (event) => {
+        if (event.target.props.uniquePrivateKey.length > 2) {
+            this.setState({ selected: event.target.props.uniquePrivateKey });
+            this.props.changeData(event.target.props.title)
+        }
     }
 
-    componentDidUpdate(){
-        document.getElementById(this.props.activeItem).classList.add("k-state-selected")
-    }
-    render(){
+    render() {
         return (
-                <PanelBar expandMode="single">
-                    <PanelBarItem expanded={true} title="Teammates">
-                        <div>
-                            <div className="teamMate k-state-selected" id="Andrew Fuller" onClick={(e) =>{ this.handleItemClick("Andrew Fuller")}}>
-                                <img src={this.imageUrl('andrew')} alt="Andrew Fuller" />
-                                <span className="mate-info">
-                                    <h2>Andrew Fuller</h2>
-                                    <p>Team Lead</p>
-                                </span>
-                            </div>
-                            <div className="teamMate" id="Nancy Leverling" onClick={() => this.handleItemClick("Nancy Leverling")}>
-                                <img src={this.imageUrl('nancy')} alt="Nancy Leverling" />
-                                <span className="mate-info">
-                                    <h2>Nancy Leverling</h2>
-                                    <p>Sales Associate</p>
-                                </span>
-                            </div>
-                            <div className="teamMate" id="Robert King" onClick={() => this.handleItemClick("Robert King")}>
-                                <img src={this.imageUrl('robert')} alt="Robert King" />
-                                <span className="mate-info">
-                                    <h2>Robert King</h2>
-                                    <p>Business System Analyst</p>
-                                </span>
-                            </div>
-                            <div className="teamMate" id="Laurence Lebihan" onClick={() => this.handleItemClick("Laurence Lebihan")}>
-                                <img src={this.imageUrl('laurence')} alt="Laurence Lebihan" />
-                                <span className="mate-info">
-                                    <h2>Laurence Lebihan</h2>
-                                    <p>Accounting Manager</p>
-                                </span>
-                            </div>
-                            <div className="teamMate" id="Mary Saveley" onClick={() => this.handleItemClick("Mary Saveley")}>
-                                <img src={this.imageUrl('mary')} alt="Mary Saveley" />
-                                <span className="mate-info">
-                                    <h2>Mary Saveley</h2>
-                                    <p>Sales Agent</p>
-                                </span>
-                            </div>
-                            <div className="teamMate" id="Hari Kumar" onClick={() => this.handleItemClick("Hari Kumar")}>
-                                <img src={this.imageUrl('hari')} alt="Hari Kumar" />
-                                <span className="mate-info">
-                                    <h2>Hari Kumar</h2>
-                                    <p>Sales Manager</p>
-                                </span>
-                            </div>
-                        </div>
-                    </PanelBarItem>
-                    <PanelBarItem title={'Sales Reports'} >
-                        <div className="project-item" id="Q1 Sales" onClick={() => this.handleItemClick("Q1 Sales")}>Q1 Sales</div>
-                        <div className="project-item" id="Q2 Sales" onClick={() => this.handleItemClick("Q2 Sales")}>Q2 Sales</div>
-                        <div className="project-item" id="Q3 Sales" onClick={() => this.handleItemClick("Q3 Sales")}>Q3 Sales</div>
-                        <div className="project-item" id="Q4 Sales" onClick={() => this.handleItemClick("Q4 Sales")}>Q4 Sales</div>
-                    </PanelBarItem>
-                    <PanelBarItem title="Sales by Day">
-                        <div className="project-item" id="Monday" onClick={() => this.handleItemClick("Monday")}>Monday</div>
-                        <div className="project-item" id="Tuesday" onClick={() => this.handleItemClick("Tuesday")}>Tuesday</div>
-                        <div className="project-item" id="Wednesday" onClick={() => this.handleItemClick("Wednesday")}>Wednesday</div>
-                        <div className="project-item" id="Thursday" onClick={() => this.handleItemClick("Thursday")}>Thursday</div>
-                        <div className="project-item" id="Friday" onClick={() => this.handleItemClick("Friday")}>Friday</div>
-                    </PanelBarItem>
-                </PanelBar>
+            <PanelBar expandMode="single" onSelect={this.handleItemSelect} selected={this.state.selected}>
+                <PanelBarItem expanded={true} title="Teammates">
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Andrew Fuller"
+                        subTitle="Team Lead"
+                        smallName="andrew"
+                        id="andrewFuller"
+                        uniquePrivateKey='.0.0' />
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Nancy Leverling"
+                        subTitle="Sales Associate"
+                        smallName="nancy"
+                        id="nancyLeverling"
+                        uniquePrivateKey='.0.1' />
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Robert King"
+                        subTitle="Business System Analyst"
+                        smallName="robert"
+                        id="robertKing"
+                        uniquePrivateKey='.0.2' />
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Laurence Lebihan"
+                        subTitle="Accounting Manager"
+                        smallName="laurence"
+                        id="laurenceLebihan"
+                        uniquePrivateKey='.0.3' />
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Mary Saveley"
+                        subTitle="Sales Agent"
+                        smallName="mary"
+                        sid="marySaveley"
+                        uniquePrivateKey='.0.4' />
+                    <CustomPanelBarItem
+                        onClick={this.handleItemSelect}
+                        selected={this.state.selected}
+                        title="Hari Kumar"
+                        subTitle="Sales Manager"
+                        smallName="hari"
+                        id="hariKumar"
+                        uniquePrivateKey='.0.5' />
+                </PanelBarItem>
+                <PanelBarItem title={'Sales Reports'} >
+                    <PanelBarItem title="Q1-sales" />
+                    <PanelBarItem title="Q2-sales" />
+                    <PanelBarItem title="Q3-sales" />
+                    <PanelBarItem title="Q4-sales" />
+                </PanelBarItem>
+                <PanelBarItem title="Sales by Day">
+                    <PanelBarItem title="Monday" />
+                    <PanelBarItem title="Tuesday" />
+                    <PanelBarItem title="Wednesday" />
+                    <PanelBarItem title="Thursday" />
+                    <PanelBarItem title="Friday" />
+                </PanelBarItem>
+            </PanelBar>
         );
     }
 }
